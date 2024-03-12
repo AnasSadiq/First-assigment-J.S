2753c0bd44569259266107967369599732d79323
AnasSadiq <156339646+AnasSadiq@users.noreply.github.com>
2024-03-12 14:26:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let first = prompt("Enter Your First Name")
+let last = prompt("Enter Your Last Name")
+let middle = prompt("Enter Your Middle Name")
+
+console.log(first)
+console.log(last)
+console.log(middle)
